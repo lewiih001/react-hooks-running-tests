e20b10800c25b16f39021aca432024880e6b8c32
Lewis <lewismunyambu@gmail.com>
2022-09-20 13:42:11 +0300

    completed assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,11 @@
 import React from "react";
-
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
+      
     </div>
   );
 }
 
-export default Article;
+export default Article
